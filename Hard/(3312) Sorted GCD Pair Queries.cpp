b26012677278e410a7644772da18525d61258a39
Harsh Kamboj <harshkamboj413@gmail.com>
2026-07-17 09:59:03 +0530

    Sorted GCD Pair Queries

---
+++ Hard/(3312) Sorted GCD Pair Queries.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        int mx = *max_element(nums.begin(), nums.end());
+        vector<int> freq(mx + 1, 0);
+        for (int x : nums) freq[x]++;
+        vector<long long> gcdCnt(mx + 1, 0);
+        for (int i = mx; i >= 1; i--) {
+            long long cnt = 0;
+            for (int j = i; j <= mx; j += i) {
+                cnt += freq[j];
+                gcdCnt[i] -= gcdCnt[j];
+            }
+            gcdCnt[i] += cnt * (cnt - 1) / 2;
+        }
+        for (int i = 2; i <= mx; i++)
+            gcdCnt[i] += gcdCnt[i - 1];
+        vector<int> ans;
+        ans.reserve(queries.size());
+        for (long long q : queries) {
+            ans.push_back(upper_bound(gcdCnt.begin(), gcdCnt.end(), q) - gcdCnt.begin());
+        }
+        return ans;
+    }
+};
